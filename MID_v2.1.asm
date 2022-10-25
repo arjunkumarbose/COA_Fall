acaaf274a0d64ffd85548f5a3acdaeaa80dfb9d1
Arjun Kumar Bose (Arnob) <82363264+arjunkumarbose@users.noreply.github.com>
2022-10-25 23:08:38 +0600

    Create MID_v2.1.asm

---
+++ MID_v2.1.asm
@@ -0,0 +1,31 @@
+.MODEL SMALL
+.STACK 100H
+.CODE
+
+MAIN PROC 
+   MOV AL,-1
+   CMP AL,0
+   
+   JL IF
+   JMP ELSE
+   
+   IF:
+   MOV AX, 31H 
+   JMP DISPLAY
+   
+   ELSE:
+   MOV AX, 30H
+   JMP DISPLAY
+   
+   DISPLAY:
+   MOV AH, 2
+   MOV DX,AX
+   INT 21H
+    
+
+EXIT:
+    MOV AH,4CH
+    INT 21H
+    
+    MAIN ENDP
+END MAIN
